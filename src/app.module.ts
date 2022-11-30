dd5bc3144ec5bb1ceaade2b12498dbc788041015
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-11-29 19:04:49 -0500

    chore: add basic app configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app/app.config';
+
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.production'],
+      load: [appConfig],
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Main');
+
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get<number>('app.port');
+
+  await app.listen(PORT, '0.0.0.0', () => {
+    logger.log(`App running on port ${PORT}`);
+  });
 }
 bootstrap();
