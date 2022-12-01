7647338043062c9a68454417e995a08ef3232bf9
dagutx <dagutmu667@gmail.com>
2022-12-01 09:54:45 -0500

    feat: improve  structure

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,24 @@
-import { IsEmail, IsNumber, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Matches,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   name: string;
 
+  @IsOptional()
   lastName: string;
 
   @IsEmail()
   email: string;
+
+  @IsString()
+  @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
+  password: string;
 
   @IsNumber()
   age: number;
