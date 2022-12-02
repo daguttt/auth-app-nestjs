49ad75589c99fe92663fa63daf2033819ee3c1fe
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-12-01 23:06:47 -0500

    fix: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   /* -***************- */
   // Validation
   app.useGlobalPipes(
