a67ec2318498a6197d8f9288092c402dabe3a823
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-12-01 21:46:48 -0500

    feat: implement simple operations with User entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -9,5 +9,10 @@ export class UsersController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    console.log();
-    console.log({ createUserDto, createUserDtoType: typeof createUserDto });
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly usersRepository: Repository<UserEntity>,
+  ) {}
 
-    return 'This method creates an user';
+  async create(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const user: UserEntity = this.usersRepository.create(createUserDto);
+    return await this.usersRepository.save(user);
+  }
+
+  async findAll() {
+    return await this.usersRepository.find();
   }
 }
